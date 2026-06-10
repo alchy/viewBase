d8177438443682120e28fab43dba6f8755cdcf9c
Jindrich Nemec <jindrich.nemec@yahoo.com>
2026-06-10 22:48:10 +0200

    feat: skeleton pip balíčku viewbase

---
+++ python/tests/test_smoke.py
@@ -0,0 +1,5 @@
+import viewbase
+
+
+def test_package_imports():
+    assert viewbase.__version__ == "0.1.0"
---
+++ python/viewbase/__init__.py
@@ -0,0 +1,3 @@
+"""viewbase – živá 2D/3D force-graph vizualizace ovládaná z Pythonu."""
+
+__version__ = "0.1.0"
